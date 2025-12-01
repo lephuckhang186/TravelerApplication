0bd80bdd859473c673a1c982cff2ec6ad0ecdc60
AKiet2808 <24127432@student.hcmus.edu.vn>
2025-12-01 15:10:16 +0700

    feat: Add OpenStreetMap widget to map screen

--- lib/features/map/screens/map_screen.dart
+++ lib/features/map/screens/map_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_map/flutter_map.dart';
+import 'package:latlong2/latlong.dart';
 
 class MapScreen extends StatefulWidget {
   const MapScreen({super.key});
@@ -11,11 +13,17 @@ class _MapScreenState extends State<MapScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Map'),
-      ),
-      body: const Center(
-        child: Text('Map Screen'),
+      body: FlutterMap(
+        options: MapOptions(
+          initialCenter: const LatLng(10.7769, 106.7009), // Ho Chi Minh City
+          initialZoom: 13.0,
+        ),
+        children: [
+          TileLayer(
+            urlTemplate: 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+            subdomains: const ['a', 'b', 'c'],
+          ),
+        ],
       ),
     );
   }
